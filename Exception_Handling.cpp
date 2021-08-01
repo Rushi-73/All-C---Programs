a4c711ea454efdcc796feb45f9e51b0d83c5bb52
Rushikesh Sonawane <70098702+Rushi-73@users.noreply.github.com>
2021-08-01 17:54:35 +0530

    Create Exception_Handling.cpp

---
+++ Exception_Handling.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+
+int main()
+{
+	float a = 10;
+	int  i;
+
+	try
+	{
+		for (i = -5; i < 5; i++)
+		{
+			if (i == 0)
+			{
+				throw(5);
+			}
+			else
+			{
+				std::cout << (a / i) << std::endl;
+			}
+		}
+	}
+	catch (int a)
+	{
+		std::cout << "Exception occur";
+	}
+	return 0;
+}

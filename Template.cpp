3c065f3e1ccaadfaadef49eb82b9f0365964a201
Rushikesh Sonawane <70098702+Rushi-73@users.noreply.github.com>
2021-09-02 08:38:44 +0530

    Update Template.cpp

--- Template.cpp
+++ Template.cpp
@@ -23,7 +23,7 @@ public:
 int main()
 {
 	Templates<int,float> obj;  //creating objects
-	std::cout<<obj.add(10, 20)<<std::endl; 
-	std::cout << obj.add(10, 20.0768f) << std::endl;
+	std::cout<<obj.add(10, 20)<<std::endl; //calling function by passing integer parameter
+	std::cout << obj.add(10, 20.0768f) << std::endl;//calling function by passing one integer and one float value
 	return 0;
 }
